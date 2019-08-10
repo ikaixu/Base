9e59fbae188d659d40bc32ea25d1ffb9de6d8580
ikaixu <olinal@163.com>
2019-08-10 17:47:25 +0800

    finish

--- src/day01/modelcode/ResizingArrayBag.java
+++ src/day01/modelcode/ResizingArrayBag.java
@@ -1,12 +1,50 @@
 package day01.modelcode;
+
 /*
  *  本例是前面学习成果的扩展，由动态数组实现一个Bag数据类型。
  *  需要先搞清楚Bag有哪些api接口 
- * 
+ *  void add(Item item)
+ *  boolean isEmpty()
+ *  int size()
  * 
  * */
 
 
-public class ResizingArrayBag {
+public class ResizingArrayBag<Item> {
+	private Item[] a; // 存储元素的数组，一个元素类型为Item的数组a。
+	private int n; // 记录存储元素的个数
+	
+	@SuppressWarnings("unchecked")
+	public ResizingArrayBag(int capacity) {
+		a = (Item[]) new Object[capacity];
+		n = 0;
+	}
+	public ResizingArrayBag() {
+		this(3);
+	}
+	
+	private void resizing(int capacity) {
+		assert capacity >= n;
+		@SuppressWarnings("unchecked")
+		Item[] temp = (Item[]) new Object[capacity];
+		for(int i = 0; i < n; i++) {
+			temp[i] = a[i];
+		}
+		a = temp;
+	}
+	
+	public void add(Item item) {
+		if(n >= a.length) resizing(2*a.length);
+		a[n] = item;
+		n++;
+	}
+	
+	public boolean isEmpty() {
+		return n == 0;
+	}
+	
+	public int size() {
+		return n;
+	}
 
 }
