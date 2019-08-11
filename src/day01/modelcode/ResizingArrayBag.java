addd10a24666fdc6b3ada0857a44e78c46f46210
ikaixu <olinal@163.com>
2019-08-11 18:45:32 +0800

    Bag增加

--- src/day01/modelcode/ResizingArrayBag.java
+++ src/day01/modelcode/ResizingArrayBag.java
@@ -1,16 +1,19 @@
 package day01.modelcode;
 
+import java.util.Iterator;
+
 /*
  *  本例是前面学习成果的扩展，由动态数组实现一个Bag数据类型。
  *  需要先搞清楚Bag有哪些api接口 
  *  void add(Item item)
  *  boolean isEmpty()
  *  int size()
- * 
+ *  如何实现迭代器接口的问题
  * */
 
 
-public class ResizingArrayBag<Item> {
+public class ResizingArrayBag<Item> implements Iterable<Item>{
+	// 为什么后面还有加入一个泛型符号，而且两个泛型符号相同。前后的Item一样吗？
 	private Item[] a; // 存储元素的数组，一个元素类型为Item的数组a。
 	private int n; // 记录存储元素的个数
 	
@@ -46,5 +49,29 @@ public class ResizingArrayBag<Item> {
 	public int size() {
 		return n;
 	}
+	
+	// 实现该接口所必须的算法
+	@Override
+	public Iterator<Item> iterator() {
+		// 需要理解方法中为什么需要泛型？？？？不理解
+		return new ReverseArrayIterator();
+	}
+	
+	private class ReverseArrayIterator implements Iterator<Item>{
+		private int i;
+		
+		public ReverseArrayIterator() {
+			i = n - 1;
+		}
 
+		@Override
+		public boolean hasNext() {
+			return i >= 0;
+		}
+
+		@Override
+		public Item next() {
+			return a[i--];
+		}
+	}
 }
